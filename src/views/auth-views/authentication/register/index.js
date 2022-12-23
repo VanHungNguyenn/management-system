383d096a6109450c28d6072f29b470daa1a8539c
Van Hung <vanhungnguyenbku@gmail.com>
2022-12-23 13:48:11 +0700

    init

---
+++ src/views/auth-views/authentication/register/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import RegisterForm from '../../components/RegisterForm'
+import { Card, Row, Col } from 'antd'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const backgroundStyle = {
+	backgroundImage: 'url(/img/others/img-17.jpg)',
+	backgroundRepeat: 'no-repeat',
+	backgroundSize: 'cover',
+}
+
+const RegisterOne = (props) => {
+	const theme = useSelector((state) => state.theme.currentTheme)
+	return (
+		<div className='h-100' style={backgroundStyle}>
+			<div className='container d-flex flex-column justify-content-center h-100'>
+				<Row justify='center'>
+					<Col xs={20} sm={20} md={20} lg={7}>
+						<Card>
+							<div className='my-2'>
+								<div className='text-center'>
+									<img
+										className='img-fluid'
+										src={`/img/${
+											theme === 'light'
+												? 'logo.png'
+												: 'logo-white.png'
+										}`}
+										alt=''
+									/>
+									<p className='text-muted'>
+										Create a new account
+									</p>
+								</div>
+								<Row justify='center'>
+									<Col xs={24} sm={24} md={20} lg={20}>
+										<RegisterForm {...props} />
+									</Col>
+								</Row>
+								{/* login */}
+								<div className='text-center'>
+									<p className='text-muted'>
+										Already have an account?
+										<Link to='/auth/login' className='ml-1'>
+											Login
+										</Link>
+									</p>
+								</div>
+							</div>
+						</Card>
+					</Col>
+				</Row>
+			</div>
+		</div>
+	)
+}
+
+export default RegisterOne
